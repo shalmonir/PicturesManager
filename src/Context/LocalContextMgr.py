09cbeba65e6bb14b54af85fbc34491fb1ee882fc
shalmonir <shalmonir@gmail.com>
2023-03-26 10:28:06 +0300

    naming change

--- src/Context/LocalContextMgr.py
+++ src/Context/LocalContextMgr.py
@@ -1,5 +1,5 @@
 from src.Context.ContextMgr import Context
-from src.DB.LocalFileDBUtility import LocalFileDBUtility
+from src.DB.LocalFileDB import LocalFileDB
 from src.Report.LocalReporter import LocalReporter
 from src.Uploaders.LocalUploader import LocalUploader
 
@@ -7,5 +7,5 @@ from src.Uploaders.LocalUploader import LocalUploader
 class LocalContextMgr(Context):
     def __init__(self):
         self.uploader = LocalUploader()
-        self.db_utility = LocalFileDBUtility()
+        self.db_utility = LocalFileDB()
         self.reporter = LocalReporter()
