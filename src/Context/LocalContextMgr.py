554efa8371e6c1c767e982026ded3858e0801fff
shalmonir <shalmonir@gmail.com>
2023-04-24 19:55:01 +0300

    Inherited class remove uploader member

--- src/Context/LocalContextMgr.py
+++ src/Context/LocalContextMgr.py
@@ -1,11 +1,9 @@
 from src.Context.ContextMgr import Context
 from src.Report.LocalReporter import LocalReporter
-from src.Uploaders.LocalUploader import LocalUploader
 from src.DB.DBUtil import DBUtil
 
 
 class LocalContextMgr(Context):
     def __init__(self):
-        self.uploader = LocalUploader()
         self.db_utility = DBUtil()
         self.reporter = LocalReporter()
