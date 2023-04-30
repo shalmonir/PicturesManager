501d82e56c1071abd70ea8a0e167f014bfa21392
shalmonir <shalmonir@gmail.com>
2023-04-30 10:26:06 +0300

    ssl capability

--- app.py
+++ app.py
@@ -4,4 +4,5 @@ app = create_app()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True, use_reloader=False)
+    context = ('local.crt', 'local.key')
+    app.run(host="0.0.0.0", debug=True, use_reloader=False, ssl_context=context)
