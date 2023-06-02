92b0e26d957e0e8d128f4e651ef9a41dbb254c1c
shalmonir <shalmonir@gmail.com>
2023-06-02 09:11:30 +0300

    nginx http->https redirection.

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(polyz.click www.polyz.click)
+domains=(polyz.click)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="shalmonir@gmail.com"
