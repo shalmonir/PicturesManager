b14496e8df13cb248cbc4ed1cdb8480ac050eab5
shalmonir <shalmonir@gmail.com>
2023-06-19 17:39:34 +0300

    common logic extraction

--- src/download.py
+++ src/download.py
@@ -6,8 +6,7 @@ from botocore.exceptions import ClientError
 from flask import Blueprint, send_from_directory, current_app, send_file
 from flask_login import login_required
 
-from src.Configuration.Configuration import AWS_VIDEO_BUCKET
-from src.Downloaders import AWSDownloader
+from src.Configuration.Configuration import AWS_VIDEO_BUCKET, AWS_FILES_BUCKET
 from src.Utils.AWSUtil import AWSUtil
 
 ERROR_RESPONSE = 'fail'
@@ -29,34 +28,34 @@ def cdn(filepath):
 @login_required
 def aws_download(filepath):
     try:
-        s3_client = AWSUtil.create_aws_s3_client()
-        file = io.BytesIO()
-        S3_BUCKET = 'nirpicturestest'
-        metadata = s3_client.head_object(Bucket=S3_BUCKET, Key=filepath)
-        conf = boto3.s3.transfer.TransferConfig(use_threads=False)
-        s3_client.download_fileobj(S3_BUCKET, filepath, file)
-        current_app.logger.debug(f"retrieving filepath: {filepath}")
+        file, metadata = get_file_from_s3(filepath, AWS_FILES_BUCKET)
         file.seek(0)
-        return send_file(file, mimetype=metadata['ContentType'])
-    except ClientError:
+        return send_file(file, mimetype=metadata)
+    except ClientError as client_error:
+        current_app.logger.error(f"retrieving filepath: {filepath}, error type 'ClientError', details: {client_error}")
         return ERROR_RESPONSE
-    except Exception:
+    except Exception as e:
+        current_app.logger.error(f"retrieving filepath: {filepath}, details: {e}")
         return ERROR_RESPONSE
 
 
 @download.route('/aws/video/<path:filepath>')
 def aws_video(filepath):
     try:
-        s3_client = AWSUtil.create_aws_s3_client()
-        file = io.BytesIO()
-        S3_BUCKET = AWS_VIDEO_BUCKET
-        metadata = s3_client.head_object(Bucket=S3_BUCKET, Key=filepath)
-        conf = boto3.s3.transfer.TransferConfig(use_threads=False)
-        s3_client.download_fileobj(S3_BUCKET, filepath, file)
-        current_app.logger.debug(f"retrieving filepath: {filepath}")
+        file, metadata = get_file_from_s3(filepath, AWS_VIDEO_BUCKET)
         file.seek(0)
-        return send_file(file, mimetype=metadata['ContentType'])
-    except ClientError:
+        return send_file(file, mimetype=metadata)
+    except ClientError as client_error:
+        current_app.logger.error(f"retrieving filepath: {filepath}, error type 'ClientError', details: {client_error}")
         return ERROR_RESPONSE
-    except Exception:
+    except Exception as e:
+        current_app.logger.error(f"retrieving filepath: {filepath}, details: {e}")
         return ERROR_RESPONSE
+
+
+def get_file_from_s3(file_path: str, bucket: str):
+    s3_client = AWSUtil.create_aws_s3_client()
+    file_stream = io.BytesIO()
+    metadata = s3_client.head_object(Bucket=bucket, Key=file_path)
+    s3_client.download_fileobj(bucket, file_path, file_stream)
+    return file_stream, metadata['ContentType']
